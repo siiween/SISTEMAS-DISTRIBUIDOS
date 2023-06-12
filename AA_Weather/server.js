910f9a16fe8b3ece6c6446740ee252e2ecb9f36b
Raúl Sigüenza <raul.siguenza@agcocorp.com>
2023-06-12 17:18:34 +0200

    Convertido weather a sockets

--- AA_Weather/server.js
+++ AA_Weather/server.js
@@ -1,13 +1,10 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
+const io = require("socket.io")(http);
 
-const citiesFilePath = 'cities.json'; // Ruta al archivo JSON con las ciudades y temperaturas
+const citiesFilePath = "cities.json";
+const citiesData = JSON.parse(fs.readFileSync(citiesFilePath, "utf8"));
 
-// Lee el archivo JSON con las ciudades y temperaturas
-const citiesData = JSON.parse(fs.readFileSync(citiesFilePath, 'utf8'));
-
-// Función para obtener una ciudad y temperatura aleatoria
 function getRandomCityTemperature() {
   const randomIndex = Math.floor(Math.random() * citiesData.length);
   return citiesData[randomIndex];
@@ -15,35 +12,32 @@ function getRandomCityTemperature() {
 
 // Crea el servidor HTTP
 const server = http.createServer((req, res) => {
-  const { pathname } = url.parse(req.url);
-
-  if (pathname === '/weather') {
-    // Obtiene una ciudad y temperatura aleatoria
-    const { city, temperature } = getRandomCityTemperature();
-
-    // Crea la respuesta en formato JSON
-    const response = {
-      city,
-      temperature
-    };
-
-    // Establece las cabeceras de la respuesta
-    res.setHeader('Content-Type', 'application/json');
-    res.writeHead(200);
-
-    // Envía la respuesta como JSON
-    res.end(JSON.stringify(response));
-  } else {
-    // Ruta no encontrada
-    res.writeHead(404);
-    res.end('Not Found');
-  }
+  res.writeHead(404);
+  res.end("Not Found");
 });
 
-// Obtiene el puerto de escucha de los argumentos de línea de comandos
+// Asocia el servidor HTTP con socket.io
+io.attach(server);
+
+// Escucha eventos de conexión de sockets
+io.on("connection", (socket) => {
+  console.log("Un cliente se ha conectado");
+
+  // Maneja el evento 'weather' para obtener una ciudad y temperatura aleatoria
+  socket.on("weather", () => {
+    const { city, temperature } = getRandomCityTemperature();
+    const response = {
+      city,
+      temperature,
+    };
+
+    // Envía la respuesta al cliente que solicitó el clima
+    socket.emit("weather", response);
+  });
+});
+
 const port = process.argv[2];
 
-// Inicia el servidor en el puerto especificado
 server.listen(port, () => {
   console.log(`Servidor de clima iniciado en el puerto ${port}`);
 });
