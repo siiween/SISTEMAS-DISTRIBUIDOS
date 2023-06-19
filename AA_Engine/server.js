6cefa02679f3a13810ae4eb87b6dd329064ff4ae
regue <daniel.regueira.ayuso@gmail.com>
2023-06-19 20:23:26 +0200

    NPC

--- AA_Engine/server.js
+++ AA_Engine/server.js
@@ -20,35 +20,12 @@ io.on("connection", (socket) => {
   });
 
   socket.on("autPlayer", ({ alias, password }) => {
-    if (!alias || !password) {
-      const atributosFaltantes = [];
-      if (!alias) atributosFaltantes.push("alias");
-      if (!password) atributosFaltantes.push("password");
-      return socket.emit("registrationError", {
-        error: "Faltan los siguientes atributos: " + atributosFaltantes.join(", "),
-      });
-    }
-  
-    // Verificar si el alias y password coinciden en la base de datos
-    db.get("SELECT id FROM jugadores WHERE alias = ? AND password = ?", [alias, password], (err, row) => {
-      if (err) {
-        console.error(err.message);
-        return socket.emit("registrationError", {
-          error: "Error en la consulta a la base de datos",
-        });
-      }
-  
-      if (!row) {
-        console.log("Alias o contraseña incorrectos");
-        return socket.emit("registrationError", {
-          error: "Alias o contraseña incorrectos",
-        });
-      }
-  
-      // El alias y password coinciden, realizar acciones adicionales si es necesario
-      console.log("Jugador autenticado correctamente");
+    if (alias === "NPC") {
+      // Saltar la autenticación para el alias "NPC"
+      console.log("Jugador autenticado como NPC");
       if (iniciarPartida) {
-        util.getMapaFromDatabase(idPartida)
+        util
+          .getMapaFromDatabase(idPartida)
           .then((mapaJSON) => {
             socket.emit("registrationSuccess", {
               status: true,
@@ -68,8 +45,57 @@ io.on("connection", (socket) => {
           message: "Partida no iniciada",
         });
       }
-    });
-  });
+    } else {
+      if (!alias || !password) {
+        const atributosFaltantes = [];
+        if (!alias) atributosFaltantes.push("alias");
+        if (!password) atributosFaltantes.push("password");
+        return socket.emit("registrationError", {
+          error: "Faltan los siguientes atributos: " + atributosFaltantes.join(", "),
+        });
+      }
+    
+      // Verificar si el alias y password coinciden en la base de datos
+      db.get("SELECT id FROM jugadores WHERE alias = ? AND password = ?", [alias, password], (err, row) => {
+        if (err) {
+          console.error(err.message);
+          return socket.emit("registrationError", {
+            error: "Error en la consulta a la base de datos",
+          });
+        }
+    
+        if (!row) {
+          console.log("Alias o contraseña incorrectos");
+          return socket.emit("registrationError", {
+            error: "Alias o contraseña incorrectos",
+          });
+        }
+    
+        // El alias y password coinciden, realizar acciones adicionales si es necesario
+        console.log("Jugador autenticado correctamente");
+        if (iniciarPartida) {
+          util.getMapaFromDatabase(idPartida)
+            .then((mapaJSON) => {
+              socket.emit("registrationSuccess", {
+                status: true,
+                message: "Partida iniciada",
+                map: mapaJSON,
+              });
+            })
+            .catch((error) => {
+              console.error("Error al recuperar el mapa:", error);
+              socket.emit("registrationError", {
+                error: "Error al recuperar el mapa",
+              });
+            });
+        } else {
+          socket.emit("registrationSuccess", {
+            status: false,
+            message: "Partida no iniciada",
+          });
+        }
+      });
+  }});
   
 });
 
